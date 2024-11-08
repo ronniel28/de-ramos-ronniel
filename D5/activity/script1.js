c3942c86c2298b34847c869d6d22ff224611adca
ronnield <ronniel.appleid@gmail.com>
2024-11-08 21:44:46 +0800

    added activity for Day 5

---
+++ D5/activity/script1.js
@@ -0,0 +1,13 @@
+let score = parseInt(prompt('Enter your score:'));
+
+if(score >= 90){
+    console.log("Grade: A"); 
+}else if(score >= 80) {
+    console.log("Grade: B"); 
+}else if(score >= 70) {
+    console.log("Grade: C"); 
+}else if(score >= 60) {
+    console.log("Grade: D"); 
+}else{
+    console.log("Grade: F"); 
+}
